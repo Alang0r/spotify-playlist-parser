47a7567265ff5e0ae06bfaff41b951df9048d381
a_gorodilov <a.gorodilov@sendy.land>
2022-07-19 18:07:08 +0300

    comments

--- main.go
+++ main.go
@@ -6,31 +6,40 @@ import (
 	"log"
 	"os"
 	lib "spotify-playlist-parser/lib"
+	"strings"
 )
 
 const (
 	filename = "Playlist1.json"
-	dirname = "My playlists"
+	dirname  = "My playlists"
 )
 
 func main() {
+	//open file
 	jsonFile, err := os.Open(filename)
 	if err != nil {
 		log.Printf("Error reading file %s: %s", filename, err)
 	}
 	defer jsonFile.Close()
 
+	//read all strings
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var file lib.File
 	json.Unmarshal(byteValue, &file)
 
+	//create directory
 	if err := os.Mkdir(dirname, os.ModePerm); err != nil {
-        log.Fatal(err)
-    }
+		log.Fatal(err)
+	}
 
 	for _, playlist := range file.Playlists {
+		//check if any "/" in names
+		playlist.Name = strings.Replace(playlist.Name, "/", "-", -1)
+
 		filename := dirname + "/" + playlist.Name + ".txt"
+
+		//generate file
 		log.Printf("Generating file: \"%s\"", filename)
 		playlistFile, err := os.Create(filename)
 		if err != nil {
@@ -38,6 +47,7 @@ func main() {
 		}
 		defer playlistFile.Close()
 
+		//fill file with songs
 		for _, track := range playlist.Items {
 			if track.LocalTrack == nil {
 				playlistFile.WriteString(track.Track.ArtistName + "-" + track.Track.TrackName + "\n")

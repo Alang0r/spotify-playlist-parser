af5a6e2bb9ed016f76755aa2ce8c550e1e3dad59
a_gorodilov <a.gorodilov@sendy.land>
2022-07-19 17:15:12 +0300

    parse

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+	lib "spotify-playlist-parser/lib"
+)
+
+
+const(
+	filename = "playlist1.json"
+)
 func main() {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Printf("Error reading file %s: %s", filename, err)
+	}
+	defer file.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var playlists []lib.Playlist
+	json.Unmarshal(byteValue, playlists)
+
+
+	
 
 }

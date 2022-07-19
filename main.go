debb4cb30c90a627185cfe944cc84bf6ce41e329
a_gorodilov <a.gorodilov@sendy.land>
2022-07-19 17:52:24 +0300

    Done!

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	filename = "playlist1.json"
+	filename = "Playlist1.json"
+	dirname = "My playlists"
 )
 
 func main() {
@@ -22,8 +23,29 @@ func main() {
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
 	var file lib.File
-	json.Unmarshal(byteValue, file)
-	
+	json.Unmarshal(byteValue, &file)
 
+	if err := os.Mkdir(dirname, os.ModePerm); err != nil {
+        log.Fatal(err)
+    }
+
+	for _, playlist := range file.Playlists {
+		filename := dirname + "/" + playlist.Name + ".txt"
+		log.Printf("Generating file: \"%s\"", filename)
+		playlistFile, err := os.Create(filename)
+		if err != nil {
+			log.Printf("Error create file %s: %s", filename, err)
+		}
+		defer playlistFile.Close()
+
+		for _, track := range playlist.Items {
+			if track.LocalTrack == nil {
+				playlistFile.WriteString(track.Track.ArtistName + "-" + track.Track.TrackName + "\n")
+			} else {
+				log.Printf("Local file found: %s", track.LocalTrack.URI)
+			}
+
+		}
+	}
 
 }
